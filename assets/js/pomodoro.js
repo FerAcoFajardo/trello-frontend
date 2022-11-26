bf9f43da0f3479e969749f468a3126e347d7249c
FerAcoFajardo <fernando.acosta216555@potros.itson.edu.mx>
2022-11-25 18:12:11 -0700

    Added counter for pomodoros and breaks
    Co-authored-by: AlexaSanchez023 <AlexaSanchez023@users.noreply.github.com>
    Co-authored-by: MortisBlack <MortisBlack@users.noreply.github.com>
    Co-authored-by: PaulBecerra <PaulBecerra@users.noreply.github.com>

--- assets/js/pomodoro.js
+++ assets/js/pomodoro.js
@@ -1,3 +1,22 @@
+const estado = document.getElementById('estado');
+const counter = document.getElementById('counter');
+const counterDescansos = document.getElementById('counter-descansos');
+
+const botonAlarma = document.getElementById("pause_alarm");
+const botonSkipBreak = document.getElementById('skip_break');
+
+const minutosPomodoro = "00";
+const segundosPomodoro = "25";
+
+const minutosDescanso = "00";
+const segundosDescanso = "25";
+
+const minutosDescansoLargo = "20";
+const segundosDescansoLargo = "00";
+
+const audio = new Audio('./assets/sounds/alarm.mp3');
+let played = false;
+
 let start = document.getElementById('start');
 let stop = document.getElementById('stop');
 let reset = document.getElementById('reset');
@@ -7,18 +26,11 @@ let ws = document.getElementById('w_seconds');
 
 let bm = document.getElementById('b_minutes');
 let bs = document.getElementById('b_seconds');
-const estado = document.getElementById('estado');
-const counter = document.getElementById('counter');
 
-const botonAlarma = document.getElementById("pause_alarm");
-const botonSkipBreak = document.getElementById('skip_break');
-botonAlarma.addEventListener('click', stopAlarm);
+//store a reference to a timer variable
+let startTimer;
 
-const minutosPomodoro = "00";
-const segundosPomodoro = "25";
-
-const minutosDescanso = "00";
-const segundosDescanso = "25";
+let startPomodoro = true;
 
 
 function disableStart(){
@@ -44,79 +56,39 @@ function enableStop(){
     estado.innerHTML = "Estado: En progreso";
 }
 
-
-//store a reference to a timer variable
-let startTimer;
-
-start.addEventListener('click', function(){
-    if(startTimer === undefined){
-        startTimer = setInterval(timer, 1000)
-    } else {
-        alert("Timer is already running");
-    }
-});
-
-botonSkipBreak.addEventListener('click', skipBreak);
-
-reset.addEventListener('click', function(){
-
-    Swal.fire({
-        title: '¿Estás seguro?',
-        text: "No podrás revertir esto!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, reiniciar!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                wm.innerText = minutosPomodoro;
-                ws.innerText = segundosPomodoro;
-
-                bm.innerText = minutosDescanso;
-                bs.innerText = segundosDescanso;
-
-                document.getElementById('counter').innerText = 4;
-                stopInterval()
-                startTimer = undefined;
-            }
-        });
-
-    
-});
-
-stop.addEventListener('click', function(){
-    stopInterval()
-    startTimer = undefined;
-});
-
-let audio = new Audio('./assets/sounds/alarm.mp3');
-let played = false;
-
-
 function playAlarm(){
     // Play the music just if is not currently playing
     if(audio.paused && !played){
+        startPomodoro = false;
         audio.play();
         played = true;
+        stopTimer();
     }
 }
+
 
 function stopAlarm(){
     audio.pause();
     audio.currentTime = 0;
+    startPomodoro = true;
+    startPomodoroTimer();
+    
 }
+
 
 function skipBreak(){
     if(wm.innerText !== 0 || ws.innerText !== 0){
         wm.innerText = minutosPomodoro;
         ws.innerText = segundosPomodoro;
-
+        
         bm.innerText = minutosDescanso;
         bs.innerText = segundosDescanso;
-        timer();
+        counter.innerText--;
+        counterDescansos.innerText++;
+        startPomodoroTimer();
     }
 }
+
 
 //Start Timer Function
 function timer(){
@@ -131,57 +103,60 @@ function timer(){
         wm.innerText--;
         wm.innerText = wm.innerText.padStart(2, '0');
     }
-
+    
     //Break Timer Countdown
-    if(wm.innerText == 0 && ws.innerText == 0){
-        if(bs.innerText != 0){
+    if(wm.innerText == 0 && ws.innerText == 0 && startPomodoro === true){
+        playAlarm();
+        if(bs.innerText != 0 && startPomodoro === true){
             bs.innerText--;
             bs.innerText = bs.innerText.padStart(2, '0');
-            playAlarm()
-        } else if(bm.innerText != 0 && bs.innerText == 0){
-            bs.innerText = segundosDescanso;
+        } else if(bm.innerText != 0 && bs.innerText == 0 && startPomodoro === true){
+            bs.innerText = segundosDescansoLargo;
             bm.innerText--;
             bm.innerText = bm.innerText.padStart(2, '0');
             
         }
     }
-
+    
     //Increment Counter by one if one full cycle is completed
     if(wm.innerText == 0 && ws.innerText == 0 && bm.innerText == 0 && bs.innerText == 0){
         played = false;
-        playAlarm();
         if(counter.innerText == 1){
             wm.innerText = minutosPomodoro;
             ws.innerText = segundosDescanso;
-
-            bm.innerText = 20;
-            bs.innerText = "00";
+            
+            bm.innerText = minutosDescansoLargo;
+            bs.innerText = segundosDescansoLargo;
         }else{
             wm.innerText = minutosPomodoro;
             ws.innerText = segundosDescanso;
-    
+            
             bm.innerText = minutosDescanso;
             bs.innerText = segundosDescanso;
-
+            
         }
         
-
+        playAlarm();
+        
         counter.innerText--;
+        counterDescansos.innerText++;
         if(counter.innerText == 0){
             counter.innerText = 5;
+            counterDescansos.innerText = 0;
         }
         played = false;
-
+        
     }
 }
 
+
 //Stop Timer Function
 function stopInterval(){
-    stopAlarm();
     enableStartButton();
     disableStop()
     clearInterval(startTimer);
 }
+
 
 function enableStartButton() {
     if(document.getElementById('list-2').children.length-1 > 0){
@@ -192,3 +167,57 @@ function enableStartButton() {
 }
 
 
+function stopTimer(){
+    stopInterval();
+    startTimer = undefined;
+}
+
+
+function startPomodoroTimer(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+}
+
+
+botonAlarma.addEventListener('click', stopAlarm);
+
+
+start.addEventListener('click',startPomodoroTimer);
+
+
+botonSkipBreak.addEventListener('click', skipBreak);
+
+
+reset.addEventListener('click', function(){
+    
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, reiniciar!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            wm.innerText = minutosPomodoro;
+            ws.innerText = segundosPomodoro;
+            
+            bm.innerText = minutosDescanso;
+            bs.innerText = segundosDescanso;
+            
+            counter.innerText = 4;
+            counterDescansos.innerText = 0;
+            stopInterval()
+            startTimer = undefined;
+        }
+    });
+
+        
+});
+
+
+stop.addEventListener('click', stopTimer);
